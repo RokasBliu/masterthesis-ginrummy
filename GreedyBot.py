52b969e007c5193b4932eccd1eee3c18b9b4b67d
RokasBliu <r.bliudzius@gmail.com>
2024-04-17 13:09:35 +0200

    Revert "Merge branch 'main' of github.com:RokasBliu/masterthesis-ginrummy"
    This reverts commit 5b13db677aa7eaffe8f095131f2ca2b5778cd414, reversing
    changes made to cf9be6335ae62a7df887e241a0daee6adb35e5c1.

--- GreedyBot.py
+++ GreedyBot.py
@@ -1,21 +1,20 @@
 import copy
-from HandEvaluator import HandEvaluator
 
 class GreedyBot:
     def __init__(self):
-        self.hand_evaluator = HandEvaluator()
+        pass
 
     def get_action(self, game_state):
         assert game_state.main_player_index == game_state.turn_index
 
         hand = game_state.main_player_hand
-        hand_deadwood = self.hand_evaluator.get_hand_score(game_state.main_player_hand)
+        hand_deadwood = game_state.hand_evaluator.get_hand_score(game_state.main_player_hand)
         best_deadwood = 100
 
         if game_state.state == "draw":
             temp_hand = copy.deepcopy(hand)
             temp_hand.add(game_state.top_card_discard_pile)
-            discard_deadwood = self.hand_evaluator.get_hand_score(temp_hand)
+            discard_deadwood = game_state.hand_evaluator.get_hand_score(temp_hand)
             if discard_deadwood < hand_deadwood:
                 return "discard"
             else:
@@ -27,7 +26,7 @@ class GreedyBot:
             for i in range(len(hand.cards)):
                 temp_hand = copy.deepcopy(hand)
                 temp_hand.cards.remove(hand.cards[i])
-                deadwood = self.hand_evaluator.get_hand_score(temp_hand)
+                deadwood = game_state.hand_evaluator.get_hand_score(temp_hand)
                 if deadwood < best_deadwood:
                     best_deadwood = deadwood
                     best_index = i
